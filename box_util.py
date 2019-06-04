f70caf9173bf32e9e31a607f0a9ca066d8af2497
drw <drw@users.noreply.github.com>
2019-06-04 11:44:20 -0400

    Add box_util.py and gadgets.py to support backing up resources to a Box account

---
+++ box_util.py
@@ -0,0 +1,159 @@
+import os, json, time
+from datetime import datetime
+from boxsdk import OAuth2, Client
+
+from parameters.local_parameters import SETTINGS_FILE
+from gadgets import open_a_channel, write_to_csv, get_all_records, get_site, get_fields, get_metadata 
+
+
+from parameters.credentials import YOUR_CLIENT_ID, YOUR_CLIENT_SECRET, YOUR_DEVELOPER_TOKEN
+auth = OAuth2(
+    client_id=YOUR_CLIENT_ID,
+    client_secret=YOUR_CLIENT_SECRET,
+    access_token=YOUR_DEVELOPER_TOKEN,
+)
+client = Client(auth)
+
+user = client.user().get()
+
+
+def create_box_folder(client,folder,folder_name):
+# https://github.com/box/box-python-sdk/blob/master/docs/usage/folders.md#create-a-folder
+    """A folder can be created by calling folder.create_subfolder(name) on the 
+    parent folder with the name of the subfolder to be created. This method 
+    returns a new Folder representing the created subfolder."""
+    #subfolder = client.folder('0').create_subfolder(folder_name) # Create sub-folder of parent folder
+    subfolder = client.folder(folder_id=folder.id).create_subfolder(folder_name) # Create sub-folder of parent folder
+    return subfolder
+
+def get_all_items_in_folder(folder):
+    """The Box SDK has a limitation; get_items calls must include a limit parameter, and 
+    apparently no more than 1000 items can be retrieved in a call. This function works
+    around this limit."""
+    all_items = []
+    offset = 0
+    next_batch = ['cookie1', 'monster2']
+    limit = 1000
+    while len(next_batch) > 0: # We could also use the total_count parameter to figure out whether
+        # all items have been obtained yet.
+        next_batch = client.folder(folder_id=folder.id).get_items(limit=limit,offset=offset)
+        all_items += next_batch
+        offset += len(next_batch)
+    return all_items
+
+def find_item_in_folder(folder,item_name,create_if_missing=False):
+    items = get_all_items_in_folder(folder)
+    for item in items:
+        if item.name == item_name:
+            return item
+    if create_if_missing:
+        new_folder = create_box_folder(client,folder,item_name)
+        return new_folder
+    else:
+        return None
+
+def find_folder_from_path(current_folder,path_parts):
+    if len(path_parts) == 0:
+        return current_folder
+    next_folder = find_item_in_folder(current_folder,path_parts[0],create_if_missing=True)
+    return find_folder_from_path(next_folder,path_parts[1:])
+
+def download_resource_file(resource_record,filename=None):
+    print("This function is only designed to download from datastores.")
+
+    # This function pulls information from a particular resource on a
+    # CKAN instance and outputs it to a CSV file.
+
+    # Tests suggest that there are some small differences between this
+    # file and the one downloaded directly through the CKAN web
+    # interface: 1) the row order is different (this version is ordered
+    # by _id (logically) while the downloaded version has the same weird
+    # order as that shown in the Data Explorer (which can have an _id
+    # sequence like 1,2,3,4,5,6,7,45,8,9,10... for no obvious reason
+    # (it may be that the data is being sorted by the order they appear
+    # in the database, which may be by their time of last update...))
+    # and 2) weird non-ASCII characters are not being handled optimally,
+    # so probably some work on character encoding is in order in the
+    # Python script.
+
+    server ='test-production'
+    site, API_key, _, _ = open_a_channel(SETTINGS_FILE, server) # API_key would only be necessary for downloading private datasets.
+
+    r_id = resource_record['resource_id']
+    if filename is None:
+        filename = "{}.csv".format(r_id)
+
+    script_path = os.path.realpath(__file__)
+    base_dir = '/'.join(script_path.split('/')[:-1])
+    path = base_dir + '/tmp/'
+    if not os.path.exists(path): 
+        os.mkdir(path)
+
+    list_of_dicts = get_all_records(site, r_id, API_key, chunk_size=5000)
+    print("len(list_of_dicts) = {}".format(len(list_of_dicts)))
+    fields = get_fields(site,r_id,API_key)
+    print("len(fields) = {}".format(len(fields)))
+    metadata = get_metadata(site,r_id,API_key)
+
+    #Eliminate _id field
+    fields.remove("_id")
+    print("The resource has the following fields: {}".format(fields))
+    filepath = path + filename
+    write_to_csv(filepath,list_of_dicts,fields)
+    metaname = filename + '-metadata.json'
+    metapath = path + metaname
+    with open(metapath, 'w', encoding='utf-8') as outfile:
+        json.dump(metadata, outfile, indent=4, sort_keys=True)
+
+    return filepath, metapath
+
+def upload_resource_to_box(destination_folder,resource_record):
+    """This function looks at the resource and decides how to get its data 
+    from the CKAN instance to Box. In some cases, this might mean downloading the
+    file to a temporary file and then uploading it. In other cases the file URL
+    from CKAN might be providable more directly to Box."""
+
+    uploaded_files = []
+    file_path, metadata_file_path = download_resource_file(resource_record)
+    # https://github.com/box/box-python-sdk/blob/master/docs/usage/files.md#upload-a-file
+    assert file_path != metadata_file_path
+    uploaded_file = destination_folder.upload(file_path, file_name=None, preflight_check=False, preflight_expected_size=0)
+    uploaded_metadata_file = destination_folder.upload(metadata_file_path, file_name=None, preflight_check=False, preflight_expected_size=0)
+    # By default, the file uploaded to Box will have the same file name as the 
+    # one on disk; you can override this by passing a different name in the 
+    # file_name parameter. You can, optionally, also choose to set a file 
+    # description upon upload by using the file_description parameter. This 
+    # method returns a File object representing the newly-uploaded file.
+
+    # [ ] To upload a file from a readable stream, call folder.upload_stream(file_stream, file_name, file_description=None, preflight_check=False, preflight_expected_size=0) with the stream and a name for the file.
+
+    # For large files or in cases where the network connection is less reliable, you may want to upload the file in parts. This allows a single part to fail without aborting the entire upload, and failed parts can then be retried.
+    # See: https://github.com/box/box-python-sdk/blob/master/docs/usage/files.md#chunked-upload
+
+    return [uploaded_file, uploaded_metadata_file]
+
+
+def backup_to_box(resource_record):
+    # Check whether last_backed_up < resource_record['last_modified'] (the parameter that I think indicates when the resource was last modified)
+    # Extract 1) resource file and 2) metadata as JSON file
+    # Store in timestamped folder
+        # data.wprdc.org-backups/<package_id>/<resource_id>/<timestamp>/
+    resource_id = resource_record['resource_id']
+    package_id = resource_record['package_id']
+    timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    path_parts = ['data.wprdc.org-backups', package_id, resource_id]
+    #root_folder = client.root_folder().get() # The root folder.
+    root_folder = client.folder('0').get()
+    resource_folder = find_folder_from_path(root_folder,path_parts)
+    destination_folder = create_box_folder(client,resource_folder,timestamp)
+    print(destination_folder.name)
+    uploaded_box_files = upload_resource_to_box(destination_folder,resource_record)
+    # Update last_backed_up field
+
+    return resource_record
+
+if __name__ == '__main__':
+    # As a test, back up the Library Locations resource.
+    resource_record = {'resource_id': '14babf3f-4932-4828-8b49-3c9a03bae6d0',
+            'package_id': '5d52127f-3606-4ed9-a019-1daf4381902a'}
+    print(backup_to_box(resource_record))
---
+++ gadgets.py
@@ -0,0 +1,179 @@
+import csv, time, json, ckanapi
+
+try:
+    from urlparse import urlparse # Python 2
+except:
+    from urllib.parse import urlparse # Python 3 renamed urlparse.
+
+def write_to_csv(filename,list_of_dicts,keys):
+    with open(filename, 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys, extrasaction='ignore', lineterminator='\n')
+        dict_writer.writeheader()
+        #for d in list_of_dicts:
+        #    dict_writer.writerow({k:v.encode('utf8') if type(v) == type('A') else v for k,v in d.items()})
+        dict_writer.writerows(list_of_dicts)
+
+def get_site(settings,server):
+    # From the dictionary obtained from ckan_settings.json,
+    # extract the URL for a particular CKAN server and return it.
+    url = settings["URLs"][server]["CKAN"]
+    scheme = urlparse(url).scheme
+    hostname = urlparse(url).hostname
+    return "{}://{}".format(scheme,hostname)
+
+def open_a_channel(settings_file,server):
+    # Get parameters to communicate with a CKAN instance
+    # from the specified JSON file.
+    with open(settings_file) as f:
+        settings = json.load(f)
+        API_key = settings["loader"][server]["ckan_api_key"]
+        site = settings["loader"][server]["ckan_root_url"]
+        package_id = settings['loader'][server]['package_id']
+    return site, API_key, package_id, settings
+
+def get_resource_data(site,resource_id,API_key=None,count=50,offset=0,fields=None):
+    # Use the datastore_search API endpoint to get <count> records from
+    # a CKAN resource starting at the given offset and only returning the
+    # specified fields in the given order (defaults to all fields in the
+    # default datastore order).
+    ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
+    if fields is None:
+        response = ckan.action.datastore_search(id=resource_id, limit=count, offset=offset)
+    else:
+        response = ckan.action.datastore_search(id=resource_id, limit=count, offset=offset, fields=fields)
+    # A typical response is a dictionary like this
+    #{u'_links': {u'next': u'/api/action/datastore_search?offset=3',
+    #             u'start': u'/api/action/datastore_search'},
+    # u'fields': [{u'id': u'_id', u'type': u'int4'},
+    #             {u'id': u'pin', u'type': u'text'},
+    #             {u'id': u'number', u'type': u'int4'},
+    #             {u'id': u'total_amount', u'type': u'float8'}],
+    # u'limit': 3,
+    # u'records': [{u'_id': 1,
+    #               u'number': 11,
+    #               u'pin': u'0001B00010000000',
+    #               u'total_amount': 13585.47},
+    #              {u'_id': 2,
+    #               u'number': 2,
+    #               u'pin': u'0001C00058000000',
+    #               u'total_amount': 7827.64},
+    #              {u'_id': 3,
+    #               u'number': 1,
+    #               u'pin': u'0001C01661006700',
+    #               u'total_amount': 3233.59}],
+    # u'resource_id': u'd1e80180-5b2e-4dab-8ec3-be621628649e',
+    # u'total': 88232}
+    data = response['records']
+    return data
+
+def get_number_of_rows(site,resource_id,API_key=None):
+    # This is pretty similar to get_fields and DRYer code might take
+    # advantage of that.
+
+    # On other/later versions of CKAN it would make sense to use
+    # the datastore_info API endpoint here, but that endpoint is
+    # broken on WPRDC.org.
+    try:
+        ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
+        results_dict = ckan.action.datastore_search(resource_id=resource_id,limit=1) # The limit
+        # must be greater than zero for this query to get the 'total' field to appear in
+        # the API response.
+        count = results_dict['total']
+    except:
+        return None
+
+    return count
+
+def get_all_records(site,resource_id,API_key=None,chunk_size=5000):
+    all_records = []
+    failures = 0
+    k = 0
+    offset = 0 # offset is almost k*chunk_size (but not quite)
+    row_count = get_number_of_rows(site,resource_id,API_key)
+    print("row count = {}".format(row_count))
+    if row_count == 0: # or if the datastore is not active
+       print("No data found in the datastore.")
+       success = False
+    while len(all_records) < row_count and failures < 5:
+        time.sleep(0.01)
+        try:
+            records = get_resource_data(site,resource_id,API_key,chunk_size,offset)
+            if records is not None:
+                all_records += records
+            failures = 0
+            offset += chunk_size
+        except:
+            failures += 1
+
+        # If the number of rows is a moving target, incorporate
+        # this step:
+        #row_count = get_number_of_rows(site,resource_id,API_key)
+        k += 1
+        print("{} iterations, {} failures, {} records, {} total records".format(k,failures,len(records) if records is not None else 0,len(all_records)))
+
+        # Another option for iterating through the records of a resource would be to
+        # just iterate through using the _links results in the API response:
+        #    "_links": {
+        #  "start": "/api/action/datastore_search?limit=5&resource_id=5bbe6c55-bce6-4edb-9d04-68edeb6bf7b1",
+        #  "next": "/api/action/datastore_search?offset=5&limit=5&resource_id=5bbe6c55-bce6-4edb-9d04-68edeb6bf7b1"
+        # Like this:
+            #if r.status_code != 200:
+            #    failures += 1
+            #else:
+            #    URL = site + result["_links"]["next"]
+
+        # Information about better ways to handle requests exceptions:
+        #http://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module/16511493#16511493
+
+    return all_records
+
+def get_fields(site,resource_id,API_key=None):
+    # In principle, it should be possible to do this using the datastore_info
+    # endpoint instead and taking the 'schema' part of the result.
+    try:
+        ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
+        results_dict = ckan.action.datastore_search(resource_id=resource_id,limit=0)
+        schema = results_dict['fields']
+        fields = [d['id'] for d in schema]
+    except:
+        return None
+
+    return fields
+
+def dealias(site,pseudonym):
+    # If a resource ID is an alias for the real resource ID, this function will
+    # convert the pseudonym into the real resource ID and return it.
+    ckan = ckanapi.RemoteCKAN(site)
+    alias_response = ckan.action.datastore_search(id='_table_metadata',filters={'name': pseudonym})
+    aliases = alias_response['records']
+    if len(aliases) > 0:
+        resource_id = aliases[0]['alias_of']
+        return resource_id
+    else:
+        return None
+
+def resource_show(ckan,resource_id):
+    # A wrapper around resource_show (which could be expanded to any resource endpoint)
+    # that tries the action, and if it fails, tries to dealias the resource ID and tries
+    # the action again.
+    try:
+        metadata = ckan.action.resource_show(id=resource_id)
+    except ckanapi.errors.NotFound:
+        # Maybe the resource_id is an alias for the real one.
+        real_id = dealias(site,resource_id)
+        metadata = ckan.action.resource_show(id=real_id)
+    except:
+        msg = "{} was not found on that CKAN instance".format(resource_id)
+        print(msg)
+        raise ckanapi.errors.NotFound(msg)
+
+    return metadata
+
+def get_metadata(site,resource_id,API_key=None):
+    try:
+        ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
+        metadata = resource_show(ckan,resource_id)
+    except:
+        return None
+
+    return metadata
